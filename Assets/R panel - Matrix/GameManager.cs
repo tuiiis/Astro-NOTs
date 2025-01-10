a2a3be462e592eaf81817b648747ce3c2f665ab3
tuiiis <tani.galaburda@gmail.com>
2025-01-10 13:34:18 +0200

    r panel motherboard
    нужно фиксить логику спавна

---
+++ Assets/R panel - Matrix/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public WireSpawnSet currentSpawnSet; // Текущая конфигурация проводов
+    public List<Transform> spawnPositions; // Координаты для спавна проводов
+    public List<Transform> holes; // Список дырок (16 штук)
+    public GameObject holePrefab; // Префаб дырки
+
+    private List<GameObject> spawnedWires = new List<GameObject>();
+    private List<GameObject> spawnedHoles = new List<GameObject>();
+
+    void Start()
+    {
+        Debug.Log(holes.Count);
+        for (int i = 0; i < holes.Count; i++)
+        {
+            Debug.Log(holes[i].transform.position.x);
+        }
+        SpawnHoles();
+        SpawnWires();
+
+    }
+
+    void SpawnHoles()
+    {
+        // Очищаем старые дырки
+        foreach (var hole in spawnedHoles)
+        {
+            Destroy(hole);
+        }
+        spawnedHoles.Clear();
+
+        // Создаём дырки
+        for (int i = 0; i < holes.Count; i++)
+        {
+            GameObject spawnedHole = Instantiate(holePrefab, holes[i].position, Quaternion.identity);
+            spawnedHoles.Add(spawnedHole);
+        }
+    }
+
+    void SpawnWires()
+    {
+        // Очищаем старые провода
+        foreach (var wire in spawnedWires)
+        {
+            Destroy(wire);
+        }
+        spawnedWires.Clear();
+
+        // Спавним провода
+        for (int i = 0; i < currentSpawnSet.wireConfigs.Count; i++)
+        {
+            // Берём позицию для спавна
+            Transform spawnPosition = spawnPositions[i];
+            WireSpawnConfig config = currentSpawnSet.wireConfigs[i];
+
+            // Создаём провод
+            GameObject spawnedWire = Instantiate(config.wirePrefab, spawnPosition.position, Quaternion.identity);
+            Debug.Log($"{spawnedWire == null} spawnedWire = NULL");
+            // Передаём информацию о правильной дырке
+            WireStart wireLogic = spawnedWire.GetComponent<WireStart>();
+            Debug.Log($"{wireLogic == null} WireLogic = NULL" );
+            Transform correctHole = holes[config.correctHoleIndex];
+            Debug.Log($"{correctHole == null} correctHole = NULL");
+            wireLogic.Setup(correctHole, holes);
+
+            spawnedWires.Add(spawnedWire);
+        }
+    }
+}
--- Assets/R panel - Matrix/WireStart.cs
+++ Assets/R panel - Matrix/WireStart.cs
@@ -1,13 +1,14 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 
 public class WireStart : MonoBehaviour
 {
     public LineRenderer lineRenderer; // Reference to the existing LineRenderer
-    public float magnitude = 0.5f;
-    public int opposite = 1;
+    public int oppositeWay = 1; // Направление смещения провода
+    public Transform correctHole; // "Правильная" дырка для подключения
 
-    private List<Transform> holes; // Dynamically retrieved list of holes
+    private float magnitude = 0.3f; // Радиус подключения
+    private List<Transform> holes; // Список дырок
     private bool isDragging = false;
     private bool isConnected = false;
     private Vector3 originalPosition;
@@ -15,25 +16,12 @@ public class WireStart : MonoBehaviour
 
     private void Start()
     {
-        originalPosition = transform.position;
-
-        // Get the list of holes from the HolesManager
-        HolesManager manager = FindObjectOfType<HolesManager>();
-        if (manager != null)
-        {
-            holes = manager.holes;
-        }
-        else
-        {
-            Debug.LogError("HolesManager not found in the scene!");
-        }
     }
 
     void Update()
     {
         if (isDragging)
         {
-            // Follow the mouse while dragging
             Vector3 mousePosition = Input.mousePosition;
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
             worldPosition.z = 0;
@@ -42,15 +30,20 @@ public class WireStart : MonoBehaviour
         }
     }
 
+    public void Setup(Transform targetHole, List<Transform> holes)
+    {
+        // Передача правильной дырки
+        correctHole = targetHole;
+        this.holes = holes;
+    }
+
     void SetPosition(Vector3 position)
     {
-        // Move the wire start point
         transform.position = position;
 
-        // Update the wire's line positions
         Vector3 positionDifference = position - lineRenderer.transform.position;
-        lineRenderer.SetPosition(1, positionDifference - new Vector3(opposite * 0.3f, 0, 0));
-        lineRenderer.SetPosition(2, positionDifference - new Vector3(opposite * 0.1f, 0, 0));
+        lineRenderer.SetPosition(1, positionDifference - new Vector3(oppositeWay * 0.3f, 0, 0));
+        lineRenderer.SetPosition(2, positionDifference - new Vector3(oppositeWay * 0.1f, 0, 0));
     }
 
     private Transform FindClosestHole(Vector3 position)
@@ -83,20 +76,31 @@ public class WireStart : MonoBehaviour
 
         if (!isConnected)
         {
-            // Find the closest hole on mouse release
             Transform closestHole = FindClosestHole(transform.position);
             if (closestHole != null)
             {
-                // Snap to the hole and finalize connection
-                SetPosition(closestHole.position);
-                isConnected = true;
-                connectedHole = closestHole;
+                if (closestHole == correctHole)
+                {
+                    SetPosition(correctHole.position);
+                    isConnected = true;
+                    connectedHole = correctHole;
+                    Debug.Log("Правильное подключение!");
+                }
+                else
+                {
+                    Debug.Log("Неправильное подключение!");
+                    ResetPosition();
+                }
             }
             else
             {
-                // Return to the original position if no valid connection
-                SetPosition(originalPosition);
+                ResetPosition();
             }
         }
+    }
+
+    private void ResetPosition()
+    {
+        SetPosition(originalPosition);
     }
 }
