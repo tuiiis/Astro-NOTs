0b8b169951ebfec356367590c457be303530aa06
tuiiis <tani.galaburda@gmail.com>
2025-01-10 13:36:31 +0200

    r panel motherboard
    нужно фиксить логику спавна

---
+++ Assets/R panel - Matrix/WireSpawnSet.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "WireSpawnSet", menuName = "Game/Wire Spawn Set")]
+public class WireSpawnSet : ScriptableObject
+{
+    public List<WireSpawnConfig> wireConfigs; // Список проводов для спавна
+}
+
